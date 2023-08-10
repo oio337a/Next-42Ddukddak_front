d929d2a7b021b01ebef78ce6cffad1102af189d8
oio337a <yongmipa@student.42seoul.kr>
2023-08-10 17:28:29 +0900

    [fix]: test

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,18 +1,18 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import JoinedDdukddak from '@/components/joinedDdukddak';
-// import Layout from '@/components/layout';
+import Layout from '@/components/layout';
 // import PrivateChatting from '@/components/privateChatting';
 // import PublicChatting from '@/components/publicChatting';
 
-// export default function Mypage() {
-//   const [showReservation, setShowReservation] = useState<number>(-1);
-//   return (
-//     <Layout>
-//       <div className="grid gap-4 py-20 px-8 xl:grid-cols-3 h-screen">
-//         <JoinedDdukddak setIndex={setShowReservation} />
-//         {/* <PrivateChatting mypage showReservation={showReservation} /> */}
-//         <PublicChatting />
-//       </div>
-//     </Layout>
-//   );
-// }
+export default function Mypage() {
+  const [showReservation, setShowReservation] = useState<number>(-1);
+  return (
+    <Layout>
+      <div className="grid gap-4 py-20 px-8 xl:grid-cols-3 h-screen">
+        {/* <JoinedDdukddak setIndex={setShowReservation} /> */}
+        {/* <PrivateChatting mypage showReservation={showReservation} /> */}
+        {/* <PublicChatting /> */}
+      </div>
+    </Layout>
+  );
+}
