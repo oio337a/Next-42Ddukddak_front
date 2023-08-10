1cf0de5663223c3ce34ba21246c45684c013dee0
oio337a <yongmipa@student.42seoul.kr>
2023-08-10 17:51:46 +0900

    [fix]: test

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
-// import JoinedDdukddak from '@/components/joinedDdukddak';
+import JoinedDdukddak from '@/components/joinedDdukddak';
 import Layout from '@/components/layout';
-// import PrivateChatting from '@/components/privateChatting';
-// import PublicChatting from '@/components/publicChatting';
+import PrivateChatting from '@/components/privateChatting';
+import PublicChatting from '@/components/publicChatting';
 
 export default function Mypage() {
   const [showReservation, setShowReservation] = useState<number>(-1);
   return (
     <Layout>
       <div className="grid gap-4 py-20 px-8 xl:grid-cols-3 h-screen">
-        {/* <JoinedDdukddak setIndex={setShowReservation} /> */}
-        {/* <PrivateChatting mypage showReservation={showReservation} /> */}
+        <JoinedDdukddak setIndex={setShowReservation} />
+        <PrivateChatting mypage showReservation={showReservation} />
         {/* <PublicChatting /> */}
       </div>
     </Layout>
--- pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function test() {
-  return (
-    <div>
-      <button>이동하기</button>
-    </div>
-  );
-}
